e6e9137e5a6e1e95352f16860eeb3325747a6852
sunny-utility-4827 <mzakzook@me.com>
2019-08-15 19:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-// function nowServing(katzDeliLine) {
-//   return katzDeliLine[0];
-//   katzDeliLine.shift;
-//   if (katzDeliLine.length == 0) {
-//     return "There is nobody waiting to be served!";
-//   }
-// }
+function nowServing(katzDeliLine) {
+  return katzDeliLine[0];
+  katzDeliLine.shift;
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!";
+  }
+}
 
 // function currentLine(curLine) {
 //   for (i = 0; i < curLine.length; i++) {
