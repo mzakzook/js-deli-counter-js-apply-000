9db19b68c38809e656c0f44e2851b5ded31229b2
sunny-utility-4827 <mzakzook@me.com>
2019-08-15 19:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-function nowServing(katzDeliLine) {
-  return katzDeliLine[0];
-  katzDeliLine.shift;
-  if (katzDeliLine.length == 0) {
-    return "There is nobody waiting to be served!";
-  }
-}
+// function nowServing(katzDeliLine) {
+//   return katzDeliLine[0];
+//   katzDeliLine.shift;
+//   if (katzDeliLine.length == 0) {
+//     return "There is nobody waiting to be served!";
+//   }
+// }
 
-function currentLine(curLine) {
-  for (i = 0; i < curLine.length; i++) {
-  var nameList 
-  nameList += `${i + 1}. ${curLine[i]}`
-  return `The line is currently: ${nameList}`
-  }
-}
+// function currentLine(curLine) {
+//   for (i = 0; i < curLine.length; i++) {
+//   var nameList 
+//   nameList += `${i + 1}. ${curLine[i]}`
+//   return `The line is currently: ${nameList}`
+//   }
+// }
 
 
