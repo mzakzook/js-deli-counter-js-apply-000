d9d130ffa510e17db75c26e7e922c01edc7b2f6a
sunny-utility-4827 <mzakzook@me.com>
2019-08-15 19:40:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(curLine) {
     return "The line is currently empty.";
   } else {
     for (i = 0; i < curLine.length; i++) {
-    nameList += `${i + 1}. ${curLine[i]}`;
+    nameList += `${i + 1}. ${curLine[i]} `;
     }
     return `The line is currently: ${nameList}`;
   }
