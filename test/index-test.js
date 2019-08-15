149f2e542ba3a77a474c7c6f72c45c9ca02f2289
sunny-utility-4827 <mzakzook@me.com>
2019-08-15 19:26:00 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -29,25 +29,25 @@ describe('deli', () => {
     });
   });
 
-//   describe('nowServing', () => {
-//     it('returns the line is empty when no one is on line', () => {
-//       expect(nowServing([])).toEqual("There is nobody waiting to be served!");
-//     });
+  describe('nowServing', () => {
+    it('returns the line is empty when no one is on line', () => {
+      expect(nowServing([])).toEqual("There is nobody waiting to be served!");
+    });
 
-//     it('returns an announcement about the person it is serving, and shifts the line', () => {
-//       const deliLine = ["Steven", "Blake", "Avi"]
-//       expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
-//       expect(deliLine).toEqual(["Blake", "Avi"]);
-//     });
-//   });
+    it('returns an announcement about the person it is serving, and shifts the line', () => {
+      const deliLine = ["Steven", "Blake", "Avi"]
+      expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
+      expect(deliLine).toEqual(["Blake", "Avi"]);
+    });
+  });
 
-//   describe('currentLine(line)', () => {
-//     it('returns "The line is currently empty." if no one is in line', () => {
-//       expect(currentLine([])).toEqual("The line is currently empty.");
-//     });
+  describe('currentLine(line)', () => {
+    it('returns "The line is currently empty." if no one is in line', () => {
+      expect(currentLine([])).toEqual("The line is currently empty.");
+    });
 
-//     it('says who is in line when there are people waiting', () => {
-//       expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
-//     });
-//   });
-// })
+    it('says who is in line when there are people waiting', () => {
+      expect(currentLine(["Bill", "Jane", "Ann"])).toEqual("The line is currently: 1. Bill, 2. Jane, 3. Ann");
+    });
+  });
+})
